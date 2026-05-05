556a54dc71eab8ab8e63dd4eec1b019ffe8360a8
Laboratório FIAP <labsfiap@fiap.com.br>
2026-05-04 22:40:09 -0300

    Exemplos dia 04/05

--- src/Exemplo01.java
+++ src/Exemplo01.java
@@ -1,5 +1,12 @@
 public class Exemplo01 {
     public static void main(String[] args) {
 
+        int cont = 1;
+
+        while(cont <= 5) {
+            System.out.println("boa noite");
+            cont = cont + 1;
+        }
+
     }
 }
